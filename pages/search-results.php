60438b9640299384bfef81ac698a1b56c400b072
ajohnson132490 <ajohnson132490@gmail.com>
2020-04-09 22:21:59 -0400

    1.034

--- pages/search-results.php
+++ pages/search-results.php
@@ -4,30 +4,12 @@
    $doc->loadHTMLFile('html/search-results.html');
    include('session.php');
 
-   //Getting the data for the current project
-   $ownr = $_SESSION['login_user'];
-   $prjct = "$ownr projects";
+   $query = $_GET['search'];
+   $sql = "SELECT * FROM login_info WHERE username = '$query'";
+   $result = mysqli_query($db,$sql);
+   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
-    // Create a result Box
-    for ($x = $row_cnt; $x >= $row_cnt; $x--) {
-
-    $resultInsert = $doc->getElementById("results");
-
-    $sql = "SELECT * FROM `$prjct` WHERE id=$x";
-    $result = mysqli_query($db,$sql2);
-    $row = mysqli_fetch_array($result2,MYSQLI_ASSOC);
-
-    //Insert a project Box
-    $resultBox = $doc->createElement('div');
-    $resultBox->setAttribute("class","resultBox");
-    $project_element_title = $doc->createElement('p', $row2["title"]);
-    $project_element_description = $doc->createElement('p', $row2["description"]);
-
-    $projectInsert->appendChild($projectBox);
-    $projectBox->appendChild($project_element_title);
-    $projectBox->appendChild($project_element_description);
-
-    }
+    echo $row['username'];
 
     echo $doc->saveXML();
 ?>
